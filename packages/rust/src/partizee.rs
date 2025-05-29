5f3b525cac10ea785bd5ba172fa9b35f91058006
MrDeadCe11 <mister.deadce11@gmail.com>
2025-05-28 21:13:42 -0500

    added missing files

---
+++ packages/rust/src/partizee.rs
@@ -0,0 +1,65 @@
+
+use clap::Parser;
+
+use std::path::PathBuf;
+
+use crate::utils::clap_cli::{Cargo, Commands};
+use crate::utils::menus::{compile_menu, deploy_menu};
+use crate::commands::new::NewProject;
+use crate::commands::compile::ProjectCompiler;
+use crate::commands::deploy::DeployProject;
+
+pub fn partizee() -> Result<(), Box<dyn std::error::Error>> {
+    let cargo_cli: Cargo = Cargo::parse();
+
+    match cargo_cli {
+        Cargo::Partizee(args) => {
+            match args.commands {
+                Commands::New { name, output_dir, zero_knowledge } => {
+                    let new_project: NewProject = NewProject::new(name, output_dir);
+                    // Pass zero_knowledge as needed
+                    new_project.create_new_project()?;
+                }
+                Commands::Compile { files_to_compile, build_args, additional_args } => {
+                    // create a new ProjectCompiler with the provided args
+                    let compile_args: ProjectCompiler = ProjectCompiler {
+                        project_root: None,
+                        files: if files_to_compile.is_some() { files_to_compile.map(|f| vec![f]) } else { None },
+                        build_args: if build_args.is_some() { build_args } else { None },
+                        additional_args: if additional_args.is_some() { additional_args } else { None },
+                    };
+                     // Use the interactive menu to get the compile args if none provided
+                        let menu_args: ProjectCompiler = compile_menu(compile_args)?;
+
+                    // create a new ProjectCompiler with the provided args
+                    let project_compiler: ProjectCompiler = ProjectCompiler::new(menu_args);
+                    // compile the contracts
+                    project_compiler.compile_contracts()?;
+                }
+                Commands::Deploy { custom_net, custom_path, custom_root, custom_deployer_args } => {
+                    let net: Option<String> = if custom_net.is_some() { Some(custom_net.unwrap()) } else { None };
+                    let path: Option<PathBuf> = if custom_path.is_some() { Some(PathBuf::from(custom_path.unwrap())) } else { None };
+                    let root: Option<PathBuf> = if custom_root.is_some() { Some(PathBuf::from(custom_root.unwrap())) } else { None };
+                    let deployer_args: Option<Vec<String>> = if custom_deployer_args.is_some() { Some(custom_deployer_args.unwrap()) } else { None };
+
+                    // create a new DeployProject with the provided args    
+                    let config = DeployProject {
+                        network: net,
+                        contract_path: path,
+                        project_root: root,
+                        deployer_args: deployer_args,
+                    };
+
+                    // get options from interactive menu and pass deployer_args as needed
+                    let menu_args: DeployProject = deploy_menu(config)?;
+                    // create a new DeployProject with the provided args
+                    let deploy_project: DeployProject = DeployProject::new(menu_args);
+                    // deploy the contract
+                    deploy_project.deploy_contracts()?;
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
